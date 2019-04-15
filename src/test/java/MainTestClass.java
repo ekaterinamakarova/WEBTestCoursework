810b6461301cd16e4ae59862af465cf5e79296c7
ekaterinamakarova <ekaterina.makarova.1999@mail.ru>
2019-04-16 00:41:40 +0300

    commit

--- src/test/java/MainTestClass.java
+++ src/test/java/MainTestClass.java
@@ -1,9 +1,39 @@
 import Pages.FileReaderClass;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
 
 public class MainTestClass {
 
     private ChromeDriver driver;
 
     FileReaderClass readerClass = new FileReaderClass();
+
+    @BeforeClass
+    public void setUp() throws IOException {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability(CapabilityType.PAGE_LOAD_STRATEGY, "normal");
+        options.addArguments("start-maximized");
+        options.addArguments("--no-sandbox");
+        driver = new ChromeDriver(options);
+        driver.get(readerClass.readFromFile(0));
+
+
+    }
+
+
+    @Test
+    public void test(){
+
+    }
+
 }
--- src/test/java/Pages/FileReaderClass.java
+++ src/test/java/Pages/FileReaderClass.java
@@ -1,4 +1,24 @@
 package Pages;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileReaderClass {
+
+    private String credentialsDirectory = "./src/credentials";
+
+    public String readFromFile(int k) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(credentialsDirectory));
+        String line;
+        List<String> lines = new ArrayList<String>();
+        while ((line = reader.readLine()) != null) {
+            lines.add(line);
+        }
+        return (lines.get(k));
+
+    }
+
 }
