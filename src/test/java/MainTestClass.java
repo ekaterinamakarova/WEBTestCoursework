8b89f34581b3ae11cf199030d3fdc19025c46580
ekaterinamakarova <ekaterina.makarova.1999@mail.ru>
2019-04-16 00:15:09 +0300

    commit

---
+++ src/test/java/MainTestClass.java
@@ -0,0 +1,9 @@
+import Pages.FileReaderClass;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MainTestClass {
+
+    private ChromeDriver driver;
+
+    FileReaderClass readerClass = new FileReaderClass();
+}
